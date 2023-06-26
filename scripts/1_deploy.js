95bcefd4af00d964708428e15e29639dc9781d82
Dirk Syburra <dsyburra&dlsdiversified.com>
2023-06-25 19:54:58 -0700

    8. Deployment

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,11 +1,12 @@
 async function main() {
     const Token = await ethers.getContractFactory("Token")
-    const token = await Token.deploy()
+    const token = await Token.deploy('Dapp University', 'DAPP', 1000000)
     await token.deployed()
     console.log(`Token Deployed to: ${token.address}`)
 }
 
 main()
+    .then(() => process.exit(0))
     .catch((error) => {
         console.error(error);
         process.exitCode = 1;
--- test/Token.js
+++ test/Token.js
@@ -11,7 +11,7 @@ describe('Token', () => {
     
     beforeEach(async () => {
         const Token = await ethers.getContractFactory('Token')
-        token = await Token.deploy('VERBmi', 'VERB' , '1000000')
+        token = await Token.deploy('Dapp University', 'DAPP' , '1000000')
         accounts = await ethers.getSigners()
         deployer = accounts[0] 
         receiver = accounts[1]
@@ -19,8 +19,8 @@ describe('Token', () => {
     })
 
     describe('Deployment', () => {
-        const name = 'VERBmi'
-        const symbol = 'VERB'
+        const name = 'Dapp University'
+        const symbol = 'DAPP'
         const decimals = '18'
         const totalSupply = tokens('1000000')
 
